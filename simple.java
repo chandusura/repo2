9e4f59a7f35fe62620581507df7ec36434d33fbd
chandusura <chandusura7@gmail.com>
2025-04-21 23:25:53 +0000

    first

--- simple.java
+++ simple.java
@@ -2,7 +2,7 @@ public class simple{
 
 public static void main(String []args){
  System.out.println("this simple java code");
- System.ou.println("hello world");
+ System.out.println("hello world");
 }
 
 }
